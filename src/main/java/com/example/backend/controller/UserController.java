17969b4ba8b7ec5e569f1aaf2d174f10af675acc
Dhanasekar <dhanasekarn0409@gmail.com>
2024-05-18 18:57:08 +0530

    Implement exception handling

--- src/main/java/com/example/backend/controller/UserController.java
+++ src/main/java/com/example/backend/controller/UserController.java
@@ -1,16 +1,15 @@
 package com.example.backend.controller;
 
+import com.example.backend.exception.UserNotFoundException;
 import com.example.backend.model.User;
 import com.example.backend.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
+@CrossOrigin("http://localhost:5173")
 public class UserController {
 	@Autowired
 	private UserRepository userRepository;
@@ -23,5 +22,11 @@ public class UserController {
 	@GetMapping("/users")
 	List<User> getAllUsers() {
 		return userRepository.findAll();
+	}
+
+	@GetMapping("/user/{id}")
+	User getUser(@PathVariable Long id) {
+		return userRepository.findById(id).
+			       orElseThrow(() -> new UserNotFoundException(id));
 	}
 }
